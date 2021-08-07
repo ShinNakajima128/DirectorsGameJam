975e286aebc80af0f261d099ab992eb67907dde1
aoyama_saito <saitomarimo1229@gmail.com>
2021-08-07 16:46:26 +0900

    射撃の機能を追加

---
+++ Assets/Aoyama/Script/Shooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float bulletTimer = 0.5f;
+    float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        Debug.Log(Timer);
+        if(Input.GetAxis("L_R_Trigger") > 0  && bulletTimer <= Timer)
+        { 
+            if (bulletPrefab)
+            {
+                GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+                bullet.transform.rotation = transform.rotation;
+                Timer = 0;
+                Debug.Log("shot!");
+            }
+        }
+    }
+}
---
+++ Assets/Aoyama/Script/twoPlayerController1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class twoPlayerController : MonoBehaviour
+{
+    [SerializeField] float m_movePower = 3f;
+    [SerializeField] float m_rotateSpeed = 1.5f;
+    Rigidbody m_rb = default;
+    Vector3 m_inputDirection = default;
+    Vector3 m_rotate = default;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float hLeft = Input.GetAxisRaw("Horizontal");
+        float vLeft = Input.GetAxisRaw("Vertical");
+        m_inputDirection = new Vector3(hLeft, 0, vLeft).normalized;
+        float hRight = Input.GetAxisRaw("HorizontalRight");
+        float vRight = Input.GetAxisRaw("VerticalRight");
+        m_rotate = new Vector3(hRight, 0, vRight).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        //Quaternion targetPosition = Quaternion.LookRotation(m_rotate);
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetPosition, Time.deltaTime * m_rotateSpeed);
+        //this.transform.up = Vector3.RotateTowards(this.transform.up, m_rotate, Time.deltaTime * m_rotateSpeed, 0);
+        Vector3 targetPos = transform.position + m_rotate;
+        transform.LookAt(targetPos, Vector3.up);
+        
+        m_rb.velocity = m_inputDirection * m_movePower;
+    }
+}
