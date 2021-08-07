d909c5113dc4fc23272c9af52bbdd9d5ca887adc
aoyama_saito <saitomarimo1229@gmail.com>
2021-08-07 16:03:40 +0900

    playerの動き一旦完成

--- Assets/Aoyama/Script/PlayerController.cs
+++ Assets/Aoyama/Script/PlayerController.cs
@@ -17,17 +17,20 @@ public class PlayerController : MonoBehaviour
     {
         float hLeft = Input.GetAxisRaw("Horizontal");
         float vLeft = Input.GetAxisRaw("Vertical");
-        m_inputDirection = new Vector3(hLeft, vLeft, 0).normalized;
+        m_inputDirection = new Vector3(hLeft, 0, vLeft).normalized;
         float hRight = Input.GetAxisRaw("HorizontalRight");
         float vRight = Input.GetAxisRaw("VerticalRight");
-        m_rotate = new Vector3(hRight, vRight, 0).normalized;
+        m_rotate = new Vector3(hRight, 0, vRight).normalized;
     }
 
     private void FixedUpdate()
     {
-        //Quaternion targetPosition
-        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation,)
-        this.transform.up = Vector3.RotateTowards(this.transform.up, m_rotate, Time.deltaTime * m_rotateSpeed, 0);
+        //Quaternion targetPosition = Quaternion.LookRotation(m_rotate);
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetPosition, Time.deltaTime * m_rotateSpeed);
+        //this.transform.up = Vector3.RotateTowards(this.transform.up, m_rotate, Time.deltaTime * m_rotateSpeed, 0);
+        Vector3 targetPos = transform.position + m_rotate;
+        transform.LookAt(targetPos, Vector3.up);
+        
         m_rb.velocity = m_inputDirection * m_movePower;
     }
 }
