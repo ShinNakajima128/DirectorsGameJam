4edc44b2d54685e07ca32e1fa9e2dbc0f7456aa5
DESKTOP-SSFGC5F\vantan <k.aki140625@icloud.com>
2021-08-07 17:28:52 +0900

    爆発のエフェクト作成途中

---
+++ Assets/kojo test/Rotate_obj.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_obj : MonoBehaviour
+{
+    [SerializeField] float m_rotate_X = 0;
+    [SerializeField] float m_rotate_Y = 0;
+    [SerializeField] float m_rotate_Z = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(m_rotate_X, m_rotate_Y, m_rotate_Z);
+    }
+}
