0da49744f797480866644068b7aaf3d914485214
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-08-08 01:35:14 +0900

    対戦が始まった時のカウントダウン機能を作成した

---
+++ Assets/Scripts/Manager/MatchManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchManager : MonoBehaviour
+{
+    [SerializeField] Text m_countText = default;
+    [SerializeField] Text m_finishText = default;
+    [SerializeField] GameObject m_finishPanel = default;
+
+    bool isFinished = true;
+    string player1Name = "<color=#FF8B72>1P</color>";
+    string player2Name = "<color=#72D9FF>2P</color>";
+    string drawText = "<color=#78FB9C>引き分け</color>";
+
+    void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    void Update()
+    {
+        if (!GameManager.Instance.InGame && !isFinished)
+        {
+            //SoundManager.Instance.PlaySeByName("");
+
+            m_finishText.enabled = true;
+            if (GameManager.Instance.Player1Win) { m_finishText.text = $"{player1Name}の勝利！"; }
+            if (GameManager.Instance.Player2Win) { m_finishText.text = $"{player2Name}の勝利！"; }
+            if (GameManager.Instance.DrawGame) { m_finishText.text = drawText; }
+
+            StartCoroutine(FinishPanel());
+
+            isFinished = true;
+            return;
+        }
+    }
+
+    public void Retry()
+    {
+        //SoundManager.Instance.PlaySeByName("");
+
+        GameManager.Instance.InGame = false;
+        GameManager.Instance.ResetFlag();
+        StartCoroutine(CountDown());
+    }
+
+    public void ReturnTitle()
+    {
+        //SoundManager.Instance.PlaySeByName("");
+
+        GameManager.Instance.ResetFlag();
+        LoadSceneManager.Instance.LoadTitleScene();
+    }
+
+    public void DebugGame()
+    {
+        GameManager.Instance.GameDebug();
+    }
+
+    IEnumerator CountDown()
+    {
+        m_finishText.text = "";
+        m_finishText.enabled = false;
+        m_finishPanel.SetActive(false);
+
+        //SoundManager.Instance.PlaySeByName("");
+        m_countText.text = "5";
+        yield return new WaitForSeconds(1.0f);
+
+        //SoundManager.Instance.PlaySeByName("");
+        m_countText.text = "4";
+        yield return new WaitForSeconds(1.0f);
+
+        //SoundManager.Instance.PlaySeByName("");
+        m_countText.text = "3";
+        yield return new WaitForSeconds(1.0f);
+
+        //SoundManager.Instance.PlaySeByName("");
+        m_countText.text = "2";
+        yield return new WaitForSeconds(1.0f);
+
+        //SoundManager.Instance.PlaySeByName("");
+        m_countText.text = "1";
+        yield return new WaitForSeconds(1.0f);
+
+        //SoundManager.Instance.PlaySeByName("");
+        GameManager.Instance.InGame = true;
+        isFinished = false;
+        m_countText.text = "START!";
+
+        yield return new WaitForSeconds(1.0f);
+        m_countText.text = "";
+    }
+
+    IEnumerator FinishPanel()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        m_finishPanel.SetActive(true);
+    }
+}
